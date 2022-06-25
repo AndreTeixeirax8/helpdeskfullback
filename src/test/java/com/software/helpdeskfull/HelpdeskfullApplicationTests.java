8941151bdf235d427fbcb19d33c30f01d5b88725
André Teixeira <andre-x8@hotmail.com>
2022-06-25 08:45:03 -0300

    Inicio do projeto

---
+++ src/test/java/com/software/helpdeskfull/HelpdeskfullApplicationTests.java
@@ -0,0 +1,13 @@
+package com.software.helpdeskfull;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelpdeskfullApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
